86a96875245863c6fd44cc3fb07e2064f9879dcd
Hjun1004 <hjun1004@naver.com>
2023-04-20 13:44:28 +0900

    MyHashMap get구현, put구효현

--- src/main/java/com/ll/exam1/MyHashMap.java
+++ src/main/java/com/ll/exam1/MyHashMap.java
@@ -1,4 +1,44 @@
 package com.ll.exam1;
 
-public class MyHashMap {
+public class MyHashMap<K, V> {
+    private int size = 0;
+    private final Object[] keys;
+    private final Object[] values;
+
+    public MyHashMap() {
+        this(2);
+    }
+
+    public MyHashMap(int arrayLength){
+        keys = new Object[arrayLength];
+        values = new Object[arrayLength];
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public V put(K key, V value) {
+        keys[size] = key;
+        values[size] = value;
+
+        size++;
+
+        return null;
+    }
+
+    public V get(K key) {
+        int indexOfKey = indexOfKey(key);
+
+        if(indexOfKey == -1) return null;
+
+        return (V)values[indexOfKey];
+    }
+
+    public int indexOfKey(K key){
+        for(int i = 0 ; i < size ; i++){
+            if(keys[i].equals(key)) return i;
+        }
+        return -1;
+    }
 }
--- src/test/java/com/ll/exam1/MyHashMapTest.java
+++ src/test/java/com/ll/exam1/MyHashMapTest.java
@@ -1,5 +1,37 @@
 package com.ll.exam1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 public class MyHashMapTest {
+
+    @Test
+    @DisplayName("size()")
+    void t1(){
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        assertThat(map.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("put()")
+    void t2(){
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        assertThat(map.put("철수", 22)).isNull();
+        assertThat(map.put("영희", 25)).isNull();
+    }
+
+    @Test
+    @DisplayName("get 구현")
+    void t3(){
+        MyHashMap<String, Integer> map = new MyHashMap<>();
+        map.put("철수", 22);
+        map.put("영희", 25);
+
+        assertThat(map.get("철수")).isEqualTo(22);
+        assertThat(map.get("영희")).isEqualTo(25);
+
+    }
 
 }
